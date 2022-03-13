5a1c3883fac7e6e6334afd03b0abc79988aec779
Daniil Smakhtsin <dansmht16@gmail.com>
2022-03-13 21:17:24 +0300

    Add plugins for auto import & configure tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,40 @@
 import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "src/dts/auto-imports.d.ts",
+    }),
+
+    Components({
+      dts: "src/dts/components.d.ts",
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
+  optimizeDeps: {
+    include: ["vue", "vue-router"],
+  },
+
+  test: {
+    include: ["src/components/**/*.test.ts"],
+    environment: "jsdom",
+    deps: {
+      inline: ["@vue"],
     },
   },
 });
