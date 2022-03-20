ccf925f8fbb6e6193c28622f8e6b606fb286bd27
Daniil Smakhtsin <dansmht16@gmail.com>
2022-03-20 10:54:18 +0300

    add some eslint rules for vue files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,4 +13,18 @@ module.exports = {
     "vue/setup-compiler-macros": true,
   },
   ignorePatterns: ["src/dts/*.d.ts"],
+  overrides: [
+    {
+      files: ["*.vue"],
+      parser: "vue-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+      rules: {
+        "no-unused-vars": "off",
+        "no-undef": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+      },
+    },
+  ],
 };
