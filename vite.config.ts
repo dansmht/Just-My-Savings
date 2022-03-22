7e0ad404f71afca48ec435e2651ddc0b3ff8801f
Daniil Smakhtsin <dansmht16@gmail.com>
2022-03-22 10:27:59 +0300

    change folders in test config

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
 
   test: {
     globals: true,
-    include: ["src/components/**/*.test.ts"],
+    include: ["src/**/*.test.ts"],
     environment: "jsdom",
     deps: {
       inline: ["@vue", "@vueuse"],
